24440d714fbd5870b31052d030652a1a04ef8fbb
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2023-02-18 12:15:46 -0600

    chore(auth-microservice): initial state

---
+++ apps/auth-microservice/src/main.ts
@@ -0,0 +1,32 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger, NestApplicationOptions } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+
+const globalPrefix = 'api';
+const port = process.env.PORT ?? 3334;
+
+export async function bootstrap(options?: NestApplicationOptions) {
+  const app = await NestFactory.create(AppModule, options);
+
+  app.setGlobalPrefix(globalPrefix).enableShutdownHooks();
+
+  return app;
+}
+
+if (require.main === module) {
+  (async () => {
+    const app = await bootstrap();
+
+    await app.listen(port);
+
+    Logger.log(
+      `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    );
+  })();
+}

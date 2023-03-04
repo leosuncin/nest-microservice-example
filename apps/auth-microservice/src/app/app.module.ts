3d3eef64d2b283d22c8578df6f503ee69a25e123
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2023-03-04 23:05:44 +0000

    feat(auth-microservice): register auth module
    Connect to MongoDB

--- apps/auth-microservice/src/app/app.module.ts
+++ apps/auth-microservice/src/app/app.module.ts
@@ -1,10 +1,17 @@
+import { AuthModule } from '@example/auth';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env['MONGO_URL'] ?? 'mongodb://mongo:password@localhost/auth'
+    ),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

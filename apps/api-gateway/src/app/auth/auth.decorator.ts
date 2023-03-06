09cab0ca86c0a9016d9992e05b304b517beee50d
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2023-03-06 17:15:41 +0000

    fix(api-gateway): amend type error in user decorator

--- apps/api-gateway/src/app/auth/auth.decorator.ts
+++ apps/api-gateway/src/app/auth/auth.decorator.ts
@@ -1,15 +1,15 @@
 import type { User } from '@example/shared-interfaces';
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import type { Request } from 'express';
 
-declare module 'express' {
-  interface Request {
-    user: User | undefined;
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Express {
+  export interface Request {
+    user?: User | undefined;
   }
 }
 
 export const CurrentUser = createParamDecorator(
   (_: never, context: ExecutionContext) => {
-    return context.switchToHttp().getRequest<Request>().user;
+    return context.switchToHttp().getRequest<Express.Request>().user;
   }
 );
